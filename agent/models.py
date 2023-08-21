0736c502ff7d0c3ac45024716763c14c2b64dfe6
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-21 19:21:33 +0330

    str functions overrided in models

--- agent/models.py
+++ agent/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Agent(models.Model):
     name = models.CharField(max_length=30)
+    
+    def __str__(self) -> str:
+        return f"Name: {self.name} with {self.id} id"
--- vendor/models.py
+++ vendor/models.py
@@ -5,10 +5,16 @@ from agent.models import Agent
 class Vendor(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return f"{self.name} with {self.id} id"
+
 class Order(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE,blank=False,null=False)
     delivery_time = models.DurationField(blank=False,null=False)
     time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.vendor}: {self.time_stamp}"
 
 class Trip(models.Model):
     STATUS_CHOICES = [
@@ -21,15 +27,15 @@ class Trip(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
+    def __str__(self) -> str:
+        return f"order: {self.order.id}, status: {self.status}"
 
 class DelayReport(models.Model):
-    STATUS_CHOICES = [
-        ('CHECKED', 'Checked'),
-        ('NOT_ASSIGNED', 'Not Assigned'),
-        ('ASSIGNED', 'Assigned'),
-    ]
-        
+
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False, blank=False)
     agent = models.ForeignKey(Agent, on_delete=models.PROTECT, blank=True, null=True)
     is_checked = models.BooleanField(default=False)
     time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"order: {self.order.id}, status: {self.is_checked}, agent: {self.agent.id}"

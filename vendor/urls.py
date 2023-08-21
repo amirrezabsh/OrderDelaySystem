07a20c1301762c76f97ff31f63a405baeb0fc03e
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-21 14:08:15 +0330

    added urls to agent app

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from vendor import views
 
-app_name = 'vendors'
-
 urlpatterns = [
     path('report-delay/<int:order_id>/', views.report_delay, name='report_delay'),
 ]

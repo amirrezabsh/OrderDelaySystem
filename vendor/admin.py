0487f11acbf110a40bd2999a83ab9b9094ca7c58
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-21 15:50:08 +0330

    registered models

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Order, DelayReport, Trip, Vendor
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(DelayReport)
+admin.site.register(Trip)
+admin.site.register(Vendor)

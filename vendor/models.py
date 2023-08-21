f845cc982640cafdbca7b12c4a5c74110382e972
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-21 11:23:40 +0330

    vendors model completed

--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,25 @@
+# vendor/models.py
 from django.db import models
 
-# Create your models here.
+class Vendor(models.Model):
+    name = models.CharField(max_length=100)
+
+class Order(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    delivery_time = models.TimeField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+class Trip(models.Model):
+    STATUS_CHOICES = [
+        ('DELIVERED', 'Delivered'),
+        ('PICKED', 'Picked'),
+        ('VENDOR_AT', 'Vendor At'),
+        ('ASSIGNED', 'Assigned'),
+    ]
+    
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+
+class DelayReport(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)

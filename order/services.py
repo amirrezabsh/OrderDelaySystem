7e390f6af89fd3f73b6f0f0a22993dc31eed1e73
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-22 20:38:47 +0330

    added more comments

--- order/services.py
+++ order/services.py
@@ -12,19 +12,23 @@ class DelayService:
     @staticmethod
     # A service to report delay for the client
     def report_delay(order_id):
+        # Check if the order exists 
         try:
             order = Order.objects.get(id=order_id)
         except Order.DoesNotExist:
             return 'Order not found', 404, None
         
         current_time = timezone.now()
+        # Check whether the order is actually delayed or not
         if timedelta(minutes=(order.delivery_time + order.eta)) + order.time_stamp > current_time:
             return 'Cannot report delay before the estimated delivery time has passed', 400, None
         
         exists_in_queue = delays_queue.exists(queue_name, order_id)
+        # Check if the order already exists in delays queue
         if exists_in_queue == order.id: 
             return 'The order is already in the delay queue', 400, None
         
+        # Check if the delayed order is in the first conditions
         if Trip.objects.filter(status__in=[TripStatus.AT_VENDOR, TripStatus.ASSIGNED, TripStatus.PICKED], order=order).exists():
             response = requests.get('https://run.mocky.io/v3/122c2796-5df4-461c-ab75-87c1192b17f7')
             if response.status_code == 200:
@@ -36,6 +40,7 @@ class DelayService:
                 return f'Order updated with new estimated time and the report has been submitted. Your order will arrive at {order.time_stamp + timedelta(minutes=order.delivery_time)}', 201, new_estimated_delivery
             
             return 'Failed to get new estimated time', 500, None
+        # If the delayed order is in the second condition
         else:
             report = DelayReport(order=order)
             report.save()
